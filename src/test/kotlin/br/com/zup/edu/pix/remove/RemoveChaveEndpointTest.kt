d7b6270c5fda7d2954eb984b648086d189fd65b5
marciodegan <marciodegan@yahoo.com>
2021-05-28 19:44:05 -0300

    Testando - Remove Chave Pix

--- src/test/kotlin/br/com/zup/edu/pix/remove/RemoveChaveEndpointTest.kt
+++ src/test/kotlin/br/com/zup/edu/pix/remove/RemoveChaveEndpointTest.kt
@@ -2,25 +2,37 @@ package br.com.zup.edu.pix.remove
 
 import br.com.zup.edu.KeyManagerRemoveGrpcServiceGrpc
 import br.com.zup.edu.RemoveChavePixRequest
+import br.com.zup.edu.bcb.BcbClient
+import br.com.zup.edu.bcb.BcbPixDeleteRequest
+import br.com.zup.edu.bcb.BcbPixDeleteResponse
 import br.com.zup.edu.pix.*
 import io.grpc.ManagedChannel
 import io.grpc.Status
 import io.grpc.StatusRuntimeException
-import io.micronaut.context.annotation.Bean
 import io.micronaut.context.annotation.Factory
 import io.micronaut.grpc.annotation.GrpcChannel
 import io.micronaut.grpc.server.GrpcServerChannel
+import io.micronaut.http.HttpResponse
+import io.micronaut.test.annotation.MockBean
 import io.micronaut.test.extensions.junit5.annotation.MicronautTest
 import org.junit.jupiter.api.*
 import org.junit.jupiter.api.Assertions.*
+import org.mockito.BDDMockito
+import org.mockito.Mockito.mock
+import java.time.LocalDateTime
 import java.util.*
 import javax.inject.Inject
+import javax.inject.Singleton
 
 @TestMethodOrder(MethodOrderer.OrderAnnotation::class)
 @MicronautTest(transactional = false)
-internal class RemoveChaveEndpointTest(
-    val keyManager: KeyManagerRemoveGrpcServiceGrpc.KeyManagerRemoveGrpcServiceBlockingStub
-) {
+internal class RemoveChaveEndpointTest{
+
+    @Inject
+    lateinit var bcbClient: BcbClient
+
+    @field:Inject
+    lateinit var keyManager: KeyManagerRemoveGrpcServiceGrpc.KeyManagerRemoveGrpcServiceBlockingStub
 
     @field:Inject
     lateinit var repository: ChavePixRepository
@@ -51,6 +63,16 @@ internal class RemoveChaveEndpointTest(
 
     @Test @Order(1)
     fun `deve remover chave existente`() {
+
+        // cenário
+        BDDMockito.given(bcbClient.deleta(
+            "teste@teste.com", BcbPixDeleteRequest(key="teste@teste.com",participant = ContaAssociada.ITAU_UNIBANCO_ISPB)))
+                .willReturn(HttpResponse.ok(BcbPixDeleteResponse(
+            key = "teste@gmail.com",
+            participant = ContaAssociada.ITAU_UNIBANCO_ISPB,
+            deletedAt = LocalDateTime.now())
+        ))
+
         // acao
         val response = keyManager.remove(
             RemoveChavePixRequest.newBuilder()
@@ -58,6 +80,7 @@ internal class RemoveChaveEndpointTest(
                 .setClienteId(CHAVE_EXISTENTE.clienteId.toString())
                 .build()
         )
+
         //validacao
         assertEquals(CHAVE_EXISTENTE.clienteId.toString(), response.clienteId)
         assertEquals(CHAVE_EXISTENTE.id.toString(), response.pixId)
@@ -78,7 +101,7 @@ internal class RemoveChaveEndpointTest(
         //validacao
         with(exceptionThrown){
             assertEquals(Status.NOT_FOUND.code, status.code)
-            assertEquals("Chave pix ou clienteID não encontrados", status.description)
+            assertEquals("Chave pix ou clienteID não encontrado(s)", status.description)
         }
     }
 
@@ -97,13 +120,18 @@ internal class RemoveChaveEndpointTest(
         // validation
         with(exceptionThrown){
             assertEquals(Status.NOT_FOUND.code, status.code)
-            assertEquals("Chave pix ou clienteID não encontrados", status.description)
+            assertEquals("Chave pix ou clienteID não encontrado(s)", status.description)
         }
+    }
+
+    @MockBean(BcbClient::class)
+    fun bcbClient(): BcbClient? {
+        return mock(BcbClient::class.java)
     }
 
     @Factory
     class Clients {
-        @Bean
+        @Singleton
         fun blockingStub(@GrpcChannel(GrpcServerChannel.NAME) channel: ManagedChannel): KeyManagerRemoveGrpcServiceGrpc.KeyManagerRemoveGrpcServiceBlockingStub {
             return KeyManagerRemoveGrpcServiceGrpc.newBlockingStub(channel)
         }
